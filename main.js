5b888e316e3ba1402b972ea11cba3264608d1f67
teslacreator23 <135182581+teslacreator23@users.noreply.github.com>
2024-09-16 18:38:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function Setup(){
+    createCanvas(280,280)
+     }
+    console.log(){
+        quik_draw_data_set
+        Math.floor().
+            random_no  = Math.floor((Math.random() *array_1.length) +1 )
+            Element_of_array = array_1[ random_no ]
+            canvas.mouseRealesed(classifyCanvas);
+
+    }
+    
+     function Classifycanvas(){
+        classifier.classify(canvas, gotResults);
+     }
+    timer_counter= 0
+    
+    timer_check =
+    
+    drawn_sketch =
+    
+    answer_holder =
+    
+    score = 1+
+    
+    preload(){
+        classifier = ml5.imageClassifier('DoodleNet');
+    }
+     function draw(){
+    check_sketch
+    if verification of variable drawn_sketch = sketch
+     }
+     check_sketch(){
+    ramdom_number
+     }
+     updateCanvas(){
+        Math.floor()
+       ramdom_number-quick_draw_data_set
+     }
+    
+     updateCanvas(){
+     }
+    
+     ramdom_number = quick_draw_data_set
+    
+    /*Actualiza la variable answer_holder - (contenedor de respuesta) con la cadena "set". Esto es para indicar que el dibujo realizado es igual al dibujo.
